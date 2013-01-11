90d325ddd0d7658b9fea291ab8a6a2474c2e0477
Michael Lutton <michael.lutton@gmail.com>
2013-01-10 22:07:14 -0500

    Initial Project Check In

---
+++ Tests.EverCraft/CharacterTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EverCraft;
+using NUnit.Framework;
+
+namespace Tests.EverCraft
+{
+    [TestFixture]
+    public class CharacterTests
+    {
+        [Test]
+        public void CharacterShouldSupportGettingAndSettingAName()
+        {
+            var character = new Character();
+
+            character.Name = "Faceman";
+
+            Assert.AreEqual("Faceman", character.Name);
+        }
+    }
+}
